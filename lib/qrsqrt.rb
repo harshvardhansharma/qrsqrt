1f250bada80efaf2f553adfc92ee6374de64f800
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2018-08-27 12:46:47 +0530

    fast inverse sqrt impl

--- lib/qrsqrt.rb
+++ lib/qrsqrt.rb
@@ -1,4 +1,17 @@
 require "qrsqrt/version"
 
 module Qrsqrt
+  def get(number, itr = 1)
+    three_halfs = 1.5
+
+    number_uint = [number].pack('F').unpack('L')[0]
+    res = 0x5f3759df - (number_uint >> 1)
+    res = [res].pack('L').unpack('F')[0]
+
+    while itr > 1 do
+      res *= (three_halfs - 0.5 * number * res * res)
+      itr -= 1
+    end
+    res
+  end
 end
